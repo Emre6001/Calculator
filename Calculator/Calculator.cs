e3fadaf4851f2d40f439ca8441d6bfbb7aaf1189
Emre Karakaş <136231693+Emre6001@users.noreply.github.com>
2023-08-08 11:19:56 +0300

    Add files via upload

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public static class Calculator
+    {
+        public static int Add (int x, int y)
+        {
+            int number1 = x;
+            int number2 = y;
+            int answer = number1 + number2;
+            return answer;
+        }
+
+        public static int Substract(int x,int y) 
+        {
+            int number1 = x;
+            int number2 = y;
+            int answer = number1 - number2;
+            return answer;
+        }
+
+        public static int Multiply(int x, int y)
+        {
+            int number1 = x;
+            int number2 = y;
+            int answer = number1 * number2;
+            return answer;
+        }
+
+        public static int Division(int x, int y)
+        {
+            int number1 = x;
+            int number2 = y;
+            int answer = number1/ number2;
+            return answer;
+        }
+
+        public static int Modulus(int x, int y)
+        {
+            int number1 = x;
+            int number2 = y;
+            int answer = (number1 % number2);
+            return answer;
+        }
+
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Calculator");
+            while (true)
+            {
+                Calculation();
+
+                Console.WriteLine("Do you want to perform another calculation? (Y/N)");
+                string again = Console.ReadLine();
+
+                if (again.Equals("Y", StringComparison.OrdinalIgnoreCase) && again.Equals("y", StringComparison.OrdinalIgnoreCase))
+                    continue;
+                break;
+                    
+            }
+            Console.WriteLine("Thank you for using the calculator!");
+
+        }
+
+        static void Calculation()
+        {
+            int result = 0;
+
+            Console.WriteLine();
+            Console.WriteLine("Enter your first number: ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Enter your second number: ");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Choose your operation");
+            Console.WriteLine("Enter (+) for addition, (-) for subtraction, (*) for multiplication, (/) for division, and (%) for modulus");
+
+            string answer = Console.ReadLine();
+
+            switch (answer)
+            {
+                case "+":
+                    result = number1 + number2;
+                    break;
+
+                case "-":
+                    result = number1 - number2;
+                    break;
+
+                case "*":
+                    result = number1 * number2;
+                    break;
+
+                case "/":
+                    if (number2 != 0)
+                        result = number1 / number2;
+                    else
+                        Console.WriteLine("Division by zero is not allowed!");
+                    break;
+
+                case "%":
+                    if (number2 != 0)
+                        result = number1 % number2;
+                    else
+                        Console.WriteLine("Modulus by zero is not allowed!");
+                    break;
+
+                default:
+                    Console.WriteLine(answer + " is not a valid operation! Please choose +, -, /, *, %");
+                    Calculation(); 
+                    return; 
+            }
+            Console.WriteLine();
+            Console.WriteLine("The result is: " + result);
+        }
+    }
+}
